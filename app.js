a7bc5618f78fa6217b0a48a51e5b7f486db1c642
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-04-11 19:08:11 +0100

    Add files via upload
    Added functionality to save new website credentials to database.

--- app.js
+++ app.js
@@ -25,22 +25,16 @@ app.use(bodyParser.urlencoded({
 // Connect to mongoose
 mongoose.connect("mongodb://localhost:27017/passwordsDB");
 
-const accountSchema = new mongoose.Schema({
-  website: String,
-  password: String
-});
-
-const Account = mongoose.model("Account", accountSchema);
-
-
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  accounts: [accountSchema]
+  accounts: [{
+    website: {type: String},
+    password: {type: String}
+  }]
 });
 
 const User = mongoose.model("User", userSchema);
-
 
 // Keep track of logged in user
 var user = "";
@@ -77,31 +71,37 @@ app.post("/register", function(req, res) {
 
   const enteredUsername = req.body.username;
   const enteredPassword = req.body.password;
+  const retypedPassword = req.body.retypedPassword;
 
-  User.findOne({username: enteredUsername}, function(err, foundAccount) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundAccount) {
-        console.log("Account already exists");
-        res.redirect("/register");
+  if (enteredPassword != retypedPassword) {
+    console.log("Passwords must match!");
+    res.redirect("/register");
+  } else {
+    User.findOne({username: enteredUsername}, function(err, foundUser) {
+      if (err) {
+        console.log(err);
       } else {
-        const newAccount = new User({
-          username: enteredUsername,
-          password: enteredPassword
-        });
-        newAccount.save(function(err) {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("Successfully saved account to database");
-            user = enteredUsername;
-            res.redirect("/password-home");
-          }
-        });
+        if (foundUser) {
+          console.log("User already exists");
+          res.redirect("/register");
+        } else {
+          const newUser = new User({
+            username: enteredUsername,
+            password: enteredPassword
+          });
+          newUser.save(function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log("Successfully saved user to database");
+              user = enteredUsername;
+              res.redirect("/password-home");
+            }
+          });
+        }
       }
-    }
-  });
+    });
+  }
 
 });
 
@@ -111,15 +111,16 @@ app.post("/login", function(req, res) {
   const enteredUsername = req.body.username;
   const enteredPassword = req.body.password;
 
-  User.findOne({username: enteredUsername}, function(err, foundAccount) {
+  User.findOne({username: enteredUsername}, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      if (foundAccount && foundAccount.password === enteredPassword) {
+      if (foundUser && foundUser.password === enteredPassword) {
         user = enteredUsername;
         res.redirect("/password-home");
       } else {
         console.log("Incorrect credentials");
+        res.redirect("/login");
       }
     }
   });
@@ -127,11 +128,43 @@ app.post("/login", function(req, res) {
 });
 
 
-// app.post("/password-add", function(req, res) {
-//
-//
-//
-// })
+app.post("/password-add", function(req, res) {
+
+  const enteredWebsite = req.body.website;
+  const enteredPassword = req.body.password;
+  var accountExists = false;
+
+  // Find logged in user
+  User.findOne({username: user}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      const userAccounts = foundUser.accounts;
+      // Check to see if user has details saved for this website already
+      userAccounts.forEach(function(account) {
+        if (account.website === enteredWebsite) {
+          accountExists = true;
+        }
+      });
+      // Save these credentials if details do not exist for this website
+      if (!accountExists) {
+        userAccounts.push({
+          website: enteredWebsite,
+          password: enteredPassword
+        });
+        foundUser.save(function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully added new account credentials");
+            res.redirect("/password-home");
+          }
+        });
+      }
+    }
+  });
+
+});
 
 
 
