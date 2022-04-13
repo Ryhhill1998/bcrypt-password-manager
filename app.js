54201ec0fef665c35c40ca10706dde278a363874
Ryan Henzell-Hill <95857127+Ryhhill1998@users.noreply.github.com>
2022-04-13 18:15:12 +0100

    Add files via upload
    Encrypted password for user accounts using bcrypt

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// Set up sessions
 app.use(sessions({
   secret: "secret string",
   resave: false,
@@ -32,7 +33,7 @@ app.use(sessions({
 
 
 
-// Connect to mongoose
+// Connect to mongoose and create user model
 mongoose.connect("mongodb://localhost:27017/passwordsDB");
 
 const userSchema = new mongoose.Schema({
@@ -47,14 +48,13 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+
 // Keep track of logged in user
 var session;
 
-// Keep track if user just saved account
-var savedAccount = false;
-
 
 // get requests
+// get root/home route if user not logged in, otherwise get dashboard
 app.get("/", function(req, res) {
   session = req.session;
   if (session.userid) {
@@ -64,14 +64,17 @@ app.get("/", function(req, res) {
   }
 });
 
+// get page for user to register an account
 app.get("/register", function(req, res) {
   res.render("register", {feedback: ""});
 });
 
+// get page for user to login if they already have an account
 app.get("/login", function(req, res) {
   res.render("login", {feedback: ""});
 });
 
+// get dashboard if user is signed in, otherwise redirect to home route
 app.get("/dashboard", function(req, res) {
   session = req.session;
   if (session.userid) {
@@ -81,16 +84,19 @@ app.get("/dashboard", function(req, res) {
   }
 });
 
+// get page for user to save credentials to their account for different websites
 app.get("/password-add", function(req, res) {
   session = req.session;
   res.render("add", {username: session.userid, feedback: ""});
 });
 
+// get page for user to search their account for saved credentials
 app.get("/password-search", function(req, res) {
   session = req.session;
   res.render("search", {username: session.userid, website: "", password: ""});
 });
 
+// get home route when user logs out and destroy their session
 app.get("/logout", function(req, res) {
   req.session.destroy(function(err) {
     if (err) {
@@ -104,58 +110,85 @@ app.get("/logout", function(req, res) {
 
 
 // post requests
+// post user account details to register an account
 app.post("/register", function(req, res) {
 
   const enteredUsername = req.body.username;
   const enteredPassword = req.body.password;
   const retypedPassword = req.body.retypedPassword;
 
+  // checked two entered passwords match
   if (enteredPassword != retypedPassword) {
     res.render("register", {feedback: "Failure"});
   } else {
+    // check if there is already an account with entered username
     User.findOne({username: enteredUsername}, function(err, foundUser) {
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
+          // if account exists already, show alert on register page
           res.render("register", {feedback: "User exists"});
         } else {
-          const newUser = new User({
-            username: enteredUsername,
-            password: enteredPassword
-          });
-          newUser.save(function(err) {
+          // if account does not exist, create new user for DB
+          // encrypt password with bcrypt
+          bcrypt.hash(enteredPassword, saltRounds, function(err, hash) {
             if (err) {
               console.log(err);
             } else {
-              session = req.session;
-              session.userid = enteredUsername;
-              res.redirect("/dashboard");
+              // create and save user with encrypted password
+              const newUser = new User({
+                username: enteredUsername,
+                password: hash
+              });
+              newUser.save(function(err) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  // create session for user
+                  session = req.session;
+                  session.userid = enteredUsername;
+                  res.redirect("/dashboard");
+                }
+              });
             }
           });
         }
       }
     });
   }
-
 });
 
 
+// post user login details to be verified and to log user in
 app.post("/login", function(req, res) {
 
   const enteredUsername = req.body.username;
   const enteredPassword = req.body.password;
 
+  // check username exists in DB
   User.findOne({username: enteredUsername}, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
-      if (foundUser && foundUser.password === enteredPassword) {
-        session = req.session;
-        session.userid = enteredUsername;
-        res.redirect("/dashboard");
+      // if user exists, check password is correct and start session if it is
+      if (foundUser) {
+        bcrypt.compare(enteredPassword, foundUser.password, function(err, result) {
+          if (err) {
+            console.log(err);
+          } else {
+            if (result === true) {
+              session = req.session;
+              session.userid = enteredUsername;
+              res.redirect("/dashboard");
+            } else {
+              // show error if password is incorrect
+              res.render("login", {feedback: "Failure"});
+            }
+          }
+        });
       } else {
-        console.log("Incorrect credentials");
+        // show error if account does not exist
         res.render("login", {feedback: "Failure"});
       }
     }
@@ -164,12 +197,14 @@ app.post("/login", function(req, res) {
 });
 
 
+// post website credentials added to DB by user
 app.post("/password-add", function(req, res) {
 
   const username = session.userid;
   const enteredWebsite = req.body.website;
   const enteredPassword = req.body.password;
-  var accountExists = false;
+
+  var userExists = false;
 
   // Find logged in user
   User.findOne({username: username}, function(err, foundUser) {
@@ -180,12 +215,13 @@ app.post("/password-add", function(req, res) {
       // Check to see if user has details saved for this website already
       userAccounts.forEach(function(account) {
         if (account.website === enteredWebsite) {
-          accountExists = true;
-          res.render("add", {feedback: "Failure"});
+          userExists = true;
         }
       });
-      // Save these credentials if details do not exist for this website
-      if (!accountExists) {
+      if (userExists) {
+        res.render("add", {username: username, feedback: "Failure"});
+      } else {
+        // Save these credentials if details do not exist for this website
         userAccounts.push({
           website: enteredWebsite,
           password: enteredPassword
@@ -194,36 +230,44 @@ app.post("/password-add", function(req, res) {
           if (err) {
             console.log(err);
           } else {
-            savedAccount = true;
-            res.render("add", {feedback: "Success"});
+            // show user that details were saved successfully
+            res.render("add", {username: username, feedback: "Success"});
           }
         });
       }
     }
   });
-
 });
 
 
+// post user's website credentials search
 app.post("/password-search", function(req, res) {
 
   const username = session.userid;
   const enteredWebsite = req.body.website;
-  var accountExists = false;
 
+  var accountExists = false;
+  var accountPassword = "";
+
+  // find user details in DB
   User.findOne({username: username}, function(err, foundUser) {
     if (err) {
       console.log(err);
     } else {
       const userAccounts = foundUser.accounts;
+      // check searched website exists
       userAccounts.forEach(function(account) {
         if (account.website === enteredWebsite) {
-          res.render("search", {website: enteredWebsite, password: account.password});
           accountExists = true;
+          accountPassword = account.password;
         }
       });
-      if (!accountExists) {
-        res.render("search", {website: enteredWebsite, password: "Credentials not found"});
+      if (accountExists) {
+        // show details for found website
+        res.render("search", {username: username, website: enteredWebsite, password: accountPassword});
+      } else {
+        // show user credentials were not found for this website
+        res.render("search", {username: username, website: enteredWebsite, password: "Credentials not found"});
       }
     }
   });
